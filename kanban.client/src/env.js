d4b0d050c6c6ebe26cf1b1f38cc6ffcdbf0ded2e
VictorJRochaJR <vrocha897@gmail.com>
2021-06-14 13:54:13 -0600

    Project

---
+++ kanban.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'codeworksdummy.us.auth0.com'
+export const audience = 'BoiseCodeWorkDummy.com'
+export const clientId = 'DJdieoX4nC4C9F8K57G8dUA1CMv6gtNV'
