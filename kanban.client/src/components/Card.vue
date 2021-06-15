5f1988b494f6119e68def3fff9eb5c8aae3fbb66
VictorJRochaJR <vrocha897@gmail.com>
2021-06-15 13:41:24 -0600

    card controller

--- kanban.client/src/components/Card.vue
+++ kanban.client/src/components/Card.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="row">
-    <Cards v-for = "c in cards" :key="c.id" :cards="c" />
+    <Cards v-for="c in cards" :key="c.id" :cards="c" />
   </div>
 </template>
 
 <script>
-import {cardsService} from CardsService
-import{AppState} from '../AppState'
+import { cardsService } from '../services/CardsService'
+import { AppState } from '../AppState'
 import { reactive } from '@vue/reactivity'
+import { onMounted } from '@vue/runtime-core'
+import { computed } from 'vue'
 export default {
   setup() {
-      const state = reactive({
-          cards: computed(() => AppState.cards)
-      })
-   onMounted(async() => {
-       try {
-           cardService.getCardsById()     
-       } catch (error) {
-           console.log(error)
-       }
-
-   }) 
-   return state
-  
-  
+    const state = reactive({
+      cards: computed(() => AppState.cards)
+    })
+    onMounted(async() => {
+      try {
+        await cardsService.getCardsById()
+      } catch (error) {
+        console.log(error)
+      }
+    })
+    return { state }
+  }
 }
 
 </script>
