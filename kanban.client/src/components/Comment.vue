61b898fb98e8713a923db94c777de71af13391b7
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-06-15 16:02:46 -0600

    tasks and comments

---
+++ kanban.client/src/components/Comment.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Comment Component</h1>
+  <h2>{{ comment.title }}</h2>
+  <p>{{ comment.content }}</p>
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+export default {
+  props: {
+    comment: { type: Object, required: true }
+  },
+  setup() {
+    const state = reactive({
+
+    })
+    return {
+      state
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ kanban.client/src/components/Task.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Task Component</h1>
+  <h2>{{ task.title }}</h2>
+  <p>{{ task.content }}</p>
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+export default {
+  props: {
+    task: { type: Object, required: true }
+  },
+  setup() {
+    const state = reactive({
+
+    })
+    return {
+      state
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
