263c2c76dad6c02437ccf78a84a5a733f3cf9f0b
VictorJRochaJR <vrocha897@gmail.com>
2021-06-15 14:51:49 -0600

    DeleteCard

--- kanban.client/src/components/CardTemplate.vue
+++ kanban.client/src/components/CardTemplate.vue
@@ -5,13 +5,29 @@
 </template>
 
 <script>
+import { reactive } from '@vue/reactivity'
+import { cardsService } from '../services/CardsService'
+// import { AppState } from '../AppState'
+import Notification from '../utils/Notification'
 
 export default {
   props: {
     card: { type: Object, required: true }
   },
-  setup() {
-    return {}
+  setup(props) {
+    const state = reactive({
+
+    })
+    return {
+      state,
+
+      async deleteCardById() {
+        if (await Notification.confirmAction('Are you sure you want to delete?')) {
+          cardsService.deleteCardById(props.card.id)
+          console.log(props.card.id, 'deleted card')
+        }
+      }
+    }
   },
   components: {}
 }
