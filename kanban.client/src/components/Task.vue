91480fd3669cac19ff7c2f3b46d5e692039e3125
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-06-15 16:18:10 -0600

    update dan

--- kanban.client/src/components/Task.vue
+++ kanban.client/src/components/Task.vue
@@ -1,18 +1,25 @@
 <template>
-  <h1>Task Component</h1>
-  <h2>{{ task.title }}</h2>
-  <p>{{ task.content }}</p>
+  <div>
+    <h1>Task Component</h1>
+    <h2>{{ task.title }}</h2>
+    <p>{{ task.content }}</p>
+  </div>
+  <div>
+    <Comment v-for="comment in comments" :key="comment.id" :comment="comment" />
+  </div>
 </template>
 
 <script>
 import { reactive } from '@vue/reactivity'
+import { computed } from '@vue/runtime-core'
+import { AppState } from '../AppState'
 export default {
   props: {
     task: { type: Object, required: true }
   },
   setup() {
     const state = reactive({
-
+      comments: computed(() => AppState.comments)
     })
     return {
       state
