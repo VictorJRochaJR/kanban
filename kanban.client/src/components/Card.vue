fb6154ebd94f65ccc1f977043f0420313a9793da
VictorJRochaJR <vrocha897@gmail.com>
2021-06-15 12:52:55 -0600

    appstate

---
+++ kanban.client/src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    Cards
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cards: computed(() => AppState.cards)
+    }
+  },
+  setup() {
+   onMounted(async() => {
+       try {
+           cardService.getCardsById()     
+       } catch (error) {
+           console.log(error)
+       }
+   }
+
+    
+   }) 
+   return {
+       state
+   },
+  components: {}
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
