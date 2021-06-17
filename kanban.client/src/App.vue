4a35e4cdfdae642372a87a19ea9f349b13856fec
Jared VandeWater <jaredvande@gmail.com>
2021-06-16 21:43:59 -0600

    homepage and app vue

--- kanban.client/src/App.vue
+++ kanban.client/src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <header>
-    <Navbar />
   </header>
   <main>
     <router-view />
   </main>
+  <footer>
+    <div class="d-flex justify-content-center my-footer py-1">
+      <p class="p-0 m-0">
+        <a target="_blank" title="Github" href="https://github.com/JaredVandeWater"><i
+          class="mdi mdi-github align-self-center ml-2"
+          aria-hidden="true"
+        ></i></a> Daniel Hildebrandt |<a target="_blank" title="Github" href="https://github.com/JaredVandeWater"><i
+          class="mdi mdi-github align-self-center ml-2"
+          aria-hidden="true"
+        ></i></a> Jared VandeWater |<a target="_blank" title="Github" href="https://github.com/JaredVandeWater"><i
+          class="mdi mdi-18px mdi-github align-self-center ml-2"
+          aria-hidden="true"
+        ></i></a> Victor Rocha
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -21,5 +36,9 @@ export default {
 </script>
 <style lang="scss">
 @import "./assets/scss/main.scss";
+.my-footer{
+background-color: rgba(0, 0, 0, 0.876);
+color: rgba(255, 255, 255, 0.548);
+}
 
 </style>
--- kanban.client/src/pages/HomePage.vue
+++ kanban.client/src/pages/HomePage.vue
@@ -2,18 +2,28 @@
   <div v-if="!state.user.isAuthenticated" class=" container-fluid home-bg my-text">
     <div class="row justify-content-center mt-5 text-center">
       <h1>
-        Welcome to KanBan!
+        Welcome to Kanban!
       </h1>
     </div>
 
-    <div class="row fillRemaining">
-      <div class="flex-column  d-flex align-items-center my-auto p-5  my-signin">
-        Sign In
+    <div class="row mt-3">
+      <div class="col my-auto d-flex justify-content-center">
+        <button @click="login" title="Sign Out" class="myButton ">
+          Sign In
+        </button>
       </div>
     </div>
   </div>
 
   <div v-if="state.user.isAuthenticated" class="container-fluid boards-bg">
+    <div class="row">
+      <div class="col d-flex justify-content-center my-text mb-2 my-top-m">
+        <h2><u>My Kanban Boards</u></h2>
+      </div>
+      <button @click="logout" title="Sign In" class="myButton2 m-4 pos-ab">
+        Sign Out
+      </button>
+    </div>
     <div class="row justify-content-center">
       <Board v-for="b in state.boards" :key="b.id" :board="b" />
       <CreateBoard />
@@ -25,6 +35,7 @@
 
 import { computed, reactive } from '@vue/runtime-core'
 import { AppState } from '../AppState'
+import { AuthService } from '../services/AuthService'
 export default {
   name: 'Home',
   setup() {
@@ -35,14 +46,23 @@ export default {
     })
 
     return {
-      state
+      state,
+      async login() {
+        AuthService.loginWithRedirect()
+      },
+      async logout() {
+        AuthService.logout()
+      }
     }
   }
 }
 </script>
 
 <style>
-
+@import url('https://fonts.googleapis.com/css2?family=Reggae+One&display=swap');
+.pos-ab{
+  position: absolute;
+}
  .home-bg{
    background-color: black;
    background-image: url("../assets/img/shattered-island.gif");
@@ -52,10 +72,64 @@ export default {
    background-image: url("../assets/img/chalkboard.jpg");
  }
  .my-text{
+font-family: 'Reggae One', cursive;
 color: white;
 text-shadow: rgb(0, 0, 0) 2px 0px 0px, rgb(0, 0, 0) 1.75517px 0.958851px 0px, rgb(0, 0, 0) 1.0806px 1.68294px 0px, rgb(0, 0, 0) 0.141474px 1.99499px 0px, rgb(0, 0, 0) -0.832294px 1.81859px 0px, rgb(0, 0, 0) -1.60229px 1.19694px 0px, rgb(0, 0, 0) -1.97998px 0.28224px 0px, rgb(0, 0, 0) -1.87291px -0.701566px 0px, rgb(0, 0, 0) -1.30729px -1.5136px 0px, rgb(0, 0, 0) -0.421592px -1.95506px 0px, rgb(0, 0, 0) 0.567324px -1.91785px 0px, rgb(0, 0, 0) 1.41734px -1.41108px 0px, rgb(0, 0, 0) 1.92034px -0.558831px 0px;
 }
 .fillRemaining{
 height: calc(100vh - (56px + 48px + 71px));
 }
+
+.my-top-m{
+  margin-top: 5rem;
+}
+
+.myButton {
+background:linear-gradient(to bottom, #525252 5%, #000000 100%);
+background-color:#525252;
+border-radius:42px;
+border:2px solid #a8a8a8;
+display:inline-block;
+cursor:pointer;
+color:#ffffff;
+font-family:Arial;
+font-size:30px;
+font-weight:bold;
+padding:20px 60px;
+text-decoration:none;
+}
+.myButton:hover {
+background:linear-gradient(to bottom, #000000 5%, #525252 100%);
+background-color:#000000;
+
+}
+.myButton:active {
+position:relative;
+top:1px;
+}
+
+.myButton2 {
+  z-index: 3;
+background:linear-gradient(to bottom, #525252 5%, #000000 100%);
+background-color:#525252;
+border-radius:36px;
+border:1px solid #a8a8a8;
+display:inline-block;
+cursor:pointer;
+color:#ffffff;
+font-family:Arial;
+font-size:16px;
+padding:2px 12px;
+text-decoration:none;
+}
+.myButton2:hover {
+background:linear-gradient(to bottom, #000000 5%, #525252 100%);
+background-color:#000000;
+
+}
+.myButton2:active {
+position:absolute;
+top:1px;
+}
+
 </style>
